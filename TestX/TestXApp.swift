da5c94021ac81bc9e07e1705723377cadef1c590
Gerson Janhuel <gersonjanhuel@gmail.com>
2021-11-12 18:00:51 +0700

    Initial Commit

---
+++ TestX/TestXApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXApp.swift
+//  TestX
+//
+//  Created by Gerson Janhuel on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TestXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
